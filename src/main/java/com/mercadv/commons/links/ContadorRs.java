c3e27a5c3181dff69101be31ae3c611d34f35398
alekz1986 <alekznder@gmail.com>
2018-12-14 04:11:22 -0500

    Primer commit

---
+++ src/main/java/com/mercadv/commons/links/ContadorRs.java
@@ -0,0 +1,11 @@
+package com.mercadv.commons.links;
+
+public class ContadorRs {
+	
+	public static final String PERSONA = "/persona";
+	
+	public static class Persona {
+		public static final String CREAR = "/crear";
+	}
+	
+}
